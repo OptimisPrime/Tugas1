68da58eac1a74e5f06cb0d039f463e4eabb7a92e
OptimisPrime <ranggamuhammad03@gmail.com>
2022-11-18 17:17:35 +0700

    Tugas1

---
+++ Pemdas Latihan3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <string.h> 
+int main() 
+{ 
+char nim[10]; 
+char nama[30]; 
+int nilai; 
+strcpy(nim, "2212510198"); 
+strcpy(nama, "Muhamad Tubagus Rangga"); 
+nilai = 85; 
+printf("NIM \t: %s", nim); 
+printf("\nNAMA \t: %s", nama); 
+printf("\nNILAI \t: %i", nilai); 
+return 0; 
+} 
